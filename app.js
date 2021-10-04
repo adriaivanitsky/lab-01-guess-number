ae90b7a1289c7010722a6f1fd4f7277819eac541
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-04 16:43:31 -0700

    fixed eslint errors from template

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const input = document.getElementById('input'); //user's guess
 const button = document.getElementById('button');    
 const result = document.getElementById('result'); //display if user is correct or not
 const guessesRemaining = document.getElementById('guesses-remaining');
+const tryAgainButton = document.getElementById('try-again');
 // initialize global state
 
 // set event listeners 
@@ -11,17 +12,24 @@ let randomNum = Math.ceil(Math.random() * 20);
 let numGuesses = 4;
 
 button.addEventListener('click', ()=>{
-  numGuesses--;
-  guessesRemaining.textContent = numGuesses; 
-  if(input === randomNum) {
-    result.textContent = 'You win!';
-  } else if (guessesRemaining === 0) {
-      result.textContent = 'You lose :(';
-  } else if (input > randomNum){
-    result.textContent = 'Too high!';
-  } else if (input < randomNum){
-    result.textContent = 'Too low';
-  }
+    numGuesses--;
+    guessesRemaining.textContent = numGuesses; 
+    if (input === randomNum) {
+        result.textContent = 'You win!';
+    } else if (guessesRemaining === 0) {
+        result.textContent = 'You lose :(';
+        tryAgainButton.style.display = 'inline';
+    } else if (input > randomNum){
+        result.textContent = 'Too high!';
+    } else if (input < randomNum){
+        result.textContent = 'Too low';
+    } 
   
 });
   
+tryAgainButton.addEventListener('click', ()=>{
+    randomNum = Math.ceil(Math.random() * 20);
+    numGuesses = 4;
+
+
+});
