843e1b9fc52885d9f6b82fade301140b7be550d8
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-04 15:12:04 -0700

    created variables in app.js and now have to figure out what to do next to get unstuck

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // import functions and grab DOM elements
-const    
+const input = document.getElementById('input');
+const button = document.getElementById('button');    
+const result = document.getElementById('result');
 // initialize global state
 
 // set event listeners 
+
+button.addEventListener('click', ()=>{
+  
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
