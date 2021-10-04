2d35c5570515946c6121d97bb3f9f897113ddfb0
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-04 16:06:18 -0700

    added dom elements and made a button event listener with else if statements

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 // import functions and grab DOM elements
-const input = document.getElementById('input');
+const input = document.getElementById('input'); //user's guess
 const button = document.getElementById('button');    
-const result = document.getElementById('result');
+const result = document.getElementById('result'); //display if user is correct or not
+const guessesRemaining = document.getElementById('guesses-remaining');
 // initialize global state
 
 // set event listeners 
 
+let randomNum = Math.ceil(Math.rondom() * 20);
+let numGuesses = 4;
+
 button.addEventListener('click', ()=>{
+  numGuesses--;
+  guessesRemaining.textcontent = numGuesses; 
+
+  if(input === randomNum) {
+    result.textContent = 'You win!';
+  } else if (guessesRemaining === 0) {
+      result.textContent = 'You lose :(';
+  } else if (input > randomNum){
+    result.textContent = 'Too high!';
+  } else if (input < randomNum){
+    result.textContent = 'Too low';
+  }
   
-})
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+});
+  
